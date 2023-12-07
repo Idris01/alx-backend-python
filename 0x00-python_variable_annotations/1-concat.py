2b27d7ca594d66957773f19784d23089de495108
Idris Adebowale <idrys01@gmail.com>
2023-12-07 16:45:59 +0000

    add concat function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""This module define a concat function
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Joins two strings together
+
+    Arguments:
+    =========
+    str1 (string) - first string
+    str2 (string) - second string
+
+    Returns: (string) result of joining str1 and str2
+    """
+    return str1 + str2
