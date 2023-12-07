1ca678b4167d780a8deab85c25c13d6ebcbb6a65
Idris Adebowale <idrys01@gmail.com>
2023-12-07 16:38:08 +0000

    add README file and add function module

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""This module define the add function
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Add two numbers a and b
+
+    Arguments:
+    =========
+    a (float): the first number
+    b (float): the second number
+
+    Returns (float): sum of a and b
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/README.md
@@ -0,0 +1,3 @@
+# Python - Variable Annotations
+## Introduction
+This project involves variable annotations with python
