df62498f1fb767d867e670598d8b28a341801b5e
Idris Adebowale <idrys01@gmail.com>
2023-12-10 21:23:58 +0000

    add zoom function

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -5,6 +5,8 @@ from typing import Tuple, List
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """Create a zoom array
+    """
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
