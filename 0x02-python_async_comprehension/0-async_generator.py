c085b40bb073ce8d01232b736aefa8b7d24d3769
Idris Adebowale <idrys01@gmail.com>
2023-12-12 07:42:18 +0000

    add documentation to async_generator and add async_comprehension coroutine

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,6 +7,8 @@ from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
+    """Generates 10 random numbers
+    """
     for count in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
